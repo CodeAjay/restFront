957336c8451764909f86eedaed2b4a7efc9038ff
codeajay <ajay01937@gmail.com>
2024-10-09 12:38:27 +0530

    removed navigate

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useCart } from './CartContext';
-import { useNavigate } from 'react-router-dom';
 import OrderConfirmation from './OrderConfirmation';
 
 const CheckoutForm: React.FC<{ onSubmit: (message: string) => void }> = ({ onSubmit }) => {
